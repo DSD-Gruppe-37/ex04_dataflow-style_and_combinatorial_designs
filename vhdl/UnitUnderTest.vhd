67d921e3d93e56f0d636144bf3dc3afec2053e4e
Rasmus Kahr <dev@kahr.me>
2020-03-17 22:22:37 +0100

    Fjern udkommentering af test entities

--- vhdl/UnitUnderTest.vhd
+++ vhdl/UnitUnderTest.vhd
@@ -12,8 +12,8 @@ ENTITY UnitUnderTest IS
         LEDR : OUT std_logic_vector(0 DOWNTO 0)
     );
 END;
-ARCHITECTURE structural OF UnitUnderTest IS
 
+ARCHITECTURE structural OF UnitUnderTest IS
 BEGIN
     UUT0 : ENTITY bin2sevenseg
         PORT MAP
@@ -24,27 +24,26 @@ BEGIN
             Sseg(6 DOWNTO 0) => HEX0
         );
 
-    -- UUT1 : ENTITY hex_mux
-    --     PORT MAP
-    --     (
-    --         -- INPUTS
-    --         bin => SW(11 DOWNTO 0),
-    --         sel => KEY(1 DOWNTO 0),
-    --         -- OUTPUTS
-    --         tsseg(6 DOWNTO 0) => HEX0,
-    --         tsseg(13 DOWNTO 7) => HEX1,
-    --         tsseg(20 DOWNTO 14) => HEX2
-    --     );
+    UUT1 : ENTITY hex_mux
+        PORT MAP
+        (
+            -- INPUTS
+            bin => SW(11 DOWNTO 0),
+            sel => KEY(1 DOWNTO 0),
+            -- OUTPUTS
+            tsseg(6 DOWNTO 0) => HEX0,
+            tsseg(13 DOWNTO 7) => HEX1,
+            tsseg(20 DOWNTO 14) => HEX2
+        );
 
-    -- UUT2 : ENTITY table_lookup
-    --     PORT MAP
-    --     (
-    --         -- INPUTS
-    --         a => SW(2),
-    --         b => SW(1),
-    --         c => SW(0),
-    --         -- OUTPUTS
-    --         x => LEDR(0)
-    --     );
-
-END structural; -- structural
+    UUT2 : ENTITY table_lookup
+        PORT MAP
+        (
+            -- INPUTS
+            a => SW(2),
+            b => SW(1),
+            c => SW(0),
+            -- OUTPUTS
+            x => LEDR(0)
+        );
+END;
